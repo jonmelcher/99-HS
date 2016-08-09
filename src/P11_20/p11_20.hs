800dd83ed497eab701100966a3d752535dbad66e
Jonathan <jonathan@viafoura.com>
2016-08-09 04:08:04 -0400

    more problems, missing 11/12/13

--- src/P11_20/p11_20.hs
+++ src/P11_20/p11_20.hs
@@ -21,3 +21,28 @@ nplicate' n = foldr (\x acc -> (ncompose' n (x:)) acc) []
 ndrop' :: Int -> [a] -> [a]
 ndrop' 0 xs = error("out of range")
 ndrop' n xs = map (fst) $ filter ((n /=) . snd) $ zip xs (cycle [1..n])
+
+-- Problem 17
+-- (*) Split a list into two parts; the length of the first part is given.
+
+nsplit' :: Int -> [a] -> ([a], [a])
+nsplit' n xs = (take n xs, drop n xs)
+
+-- Problem 18
+-- (**) Extract a slice from a list.
+
+slice' :: Int -> Int -> [a] -> [a]
+slice' i j xs = take (j - i + 1) (drop (i - 1) xs)
+
+
+-- Problem 19
+-- (**) Rotate a list N places to the left.
+
+nrotate' :: Int -> [a] -> [a]
+nrotate' n xs = drop (mod n $ length xs) xs ++ take (mod n $ length xs) xs
+
+-- Problem 20
+-- (*) Remove the K'th element from a list.
+
+drop' :: Int -> [a] -> (a, [a])
+drop' k xs = (xs !! k, take (k - 1) xs ++ drop k xs)
